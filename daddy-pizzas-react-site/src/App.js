36513c8f33ef202c1f7f3fc5505b5160bc5037c9
IgmoRqq <vagazz405@gmail.com>
2024-06-04 21:51:28 +0300

    react загрузка проекта + привязка ручек API

---
+++ daddy-pizzas-react-site/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Login from './components/Login'; 
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Daddy's Pizzas!</h1>
+            <Login /> 
+        </div>
+    );
+}
+
+export default App;
